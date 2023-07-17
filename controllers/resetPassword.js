5fc6feb444f21d9872d3918e6ad5421ef408c69b
Soumajith Das <91211431+Soumajith@users.noreply.github.com>
2023-07-17 11:26:28 +0530

    ReseT password API completed

--- controllers/resetPassword.js
+++ controllers/resetPassword.js
@@ -1,6 +1,7 @@
 // packages
 const User = require("../models/User");
 const mailSender = require("../utils/mailSender");
+const bcrypt = require("bcrypt");
 // reset password token
 
 exports.resetPasswordToken = async (request, response) => {
@@ -60,12 +61,52 @@ exports.resetPasswordToken = async (request, response) => {
 exports.resetPassword = async (request, response) => {
   try {
     // fetch
+    const { password, confirmPassword, token } = request.body;
     // validate
+    if (password !== confirmPassword) {
+      return response.status(401).json({
+        success: false,
+        message: "Password did not match",
+      });
+    }
+
     // get the userdetail using token
-    // validate token
-    // token time check
+    const userDetails = await User.findOne({ token: token });
+    if (!userDetails) {
+      return response.status(404).json({
+        success: false,
+        message: "Token not found",
+      });
+    }
+
+    // validate token time
+    if (userDetails.expiresIn < Date.now()) {
+      return response.status(400).json({
+        success: false,
+        message: "Token expired. Try again",
+      });
+    }
+
     // hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     // update
+    await User.findOneAndUpdate(
+      { token: token },
+      { password: hashedPassword },
+      { new: true }
+    );
+
     // response
-  } catch (err) {}
+    response.status(200).json({
+      success: true,
+      message: "Password changed successfully",
+    });
+  } catch (err) {
+    console.log(err);
+    response.status(500).json({
+      success: false,
+      message: "Something went wrong. Try again",
+    });
+  }
 };
