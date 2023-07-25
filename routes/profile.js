86a47b2dde182173e3c4d9402bae741e870da9f3
Soumajith Das <91211431+Soumajith@users.noreply.github.com>
2023-07-25 20:48:29 +0530

    Added Contact us API

--- routes/profile.js
+++ routes/profile.js
@@ -2,5 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 // Import
+const {
+  editProfile,
+  getProfileDetails,
+  deleteAccount,
+} = require("../controllers/profile");
 
 // queries
+router.post("/editProfile", editProfile);
+router.get("/getProfileDetails", getProfileDetails);
+router.post("/deleteAccount", deleteAccount);
+
+module.exports = router;
