d8346ca191f9c325852f5ba14d51a068ef1ae42a
Soumajith Das <91211431+Soumajith@users.noreply.github.com>
2023-07-25 14:00:23 +0530

    Added SubSection delete and update controller

--- controllers/subSection.js
+++ controllers/subSection.js
@@ -54,6 +54,87 @@ exports.editSubSection = async (request, response) => {
   try {
     const { subSectionId, title, description } = request.body;
     const { sectionId } = request.params;
-  } catch (err) {}
+    const videoFile = request.files.videoFile;
+
+    if (!subSectionId || !sectionId || !title || !description) {
+      return response.status(400).json({
+        success: false,
+        message: "Empty fields",
+      });
+    }
+
+    const subSection = await SubSection.findById(subSectionId);
+    if (!subSection) {
+      return response.status(404).json({
+        success: false,
+        message: "Subsection not found",
+      });
+    }
+
+    subSection.title = title;
+    subSection.description = description;
+
+    if (videoFile !== undefined) {
+      const uploadResponse = await cloudinaryUpload(
+        videoFile,
+        process.env.FOLDER_NAME
+      );
+      subSection.videoUrl = uploadResponse.secure_url;
+      subSection.duration = `${uploadResponse.duration}`;
+    }
+
+    await subSection.save();
+
+    const updatedSection = await Section.findById(sectionId)
+      .populate("subSection")
+      .exec();
+
+    return response.status(200).json({
+      success: true,
+      message: "Sub Section updated successfully",
+      data: updatedSection,
+    });
+  } catch (err) {
+    console.log(err);
+    return response.status(500).json({
+      success: false,
+      message: "Server error, try again",
+      error: err.message,
+    });
+  }
 };
+
 // delete
+exports.deleteSubSection = async (request, response) => {
+  try {
+    const { subSectionId } = request.body;
+    const { sectionId } = request.params;
+
+    const subSection = await SubSection.findByIdAndDelete(subSectionId);
+    if (!subSection) {
+      return response.status(404).json({
+        success: false,
+        message: "Sub Section not found",
+      });
+    }
+
+    const updatedSection = await Section.findByIdAndUpdate(
+      sectionId,
+      { $pull: { subSection: subSectionId } },
+      { new: true }
+    );
+
+    response.status(200).json({
+      success: true,
+      message: "Sub Section deleted successfully",
+      data: updatedSection,
+    });
+  } catch (err) {
+    console.log(err);
+    return response.status(500).json({
+      success: false,
+      message: "Server error",
+      error: err.message,
+    });
+  }
+};
